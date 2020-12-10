83fd604d478fcf25cbd34916f1ccf7fb47c56199
Nadeen Gamage <nadeengamage@gmail.com>
2020-12-11 02:44:40 +0530

    student get by nic id

--- api/students/controllers/students.js
+++ api/students/controllers/students.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    
+    async findOne(ctx) {
+        const { nic_id } = ctx.params;
+    
+        const entity = await strapi.services.students.findOne({ nic_id: nic_id });
+        return sanitizeEntity(entity, { model: strapi.models.students });
+      }
+};
